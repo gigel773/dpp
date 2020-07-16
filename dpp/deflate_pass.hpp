ff3fedc2b9415bec8806cee19b633e4e7c503b66
Gigel773 <gigelbig@gmail.com>
2020-07-16 23:13:49 +0300

    [refactoring][deflate] Fix of offset overflow

--- dpp/deflate_pass.hpp
+++ dpp/deflate_pass.hpp
@@ -51,89 +51,16 @@ namespace dpp
                    *left == *right)
             {
                 match_length++;
+                left++;
+                right++;
             }
 
             return match_length >= MINIMAL_MATCH_LENGTH ? match_length : -1;
         }
 
-        auto get_length_index(int32_t length) -> uint32_t
-        {
-            if (length < 11)
-            {
-                return 257 + length - 3;
-            } else if (length < 19)
-            {
-                return 261 + (length - 3) / 2;
-            } else if (length < 35)
-            {
-                return 265 + (length - 3) / 4;
-            } else if (length < 67)
-            {
-                return 269 + (length - 3) / 8;
-            } else if (length < 131)
-            {
-                return 273 + (length - 3) / 16;
-            } else if (length < 258)
-            {
-                return 277 + (length - 3) / 32;
-            } else
-            {
-                return 285;
-            }
-        }
+        auto get_length_index(int32_t length) -> uint32_t;
 
-        auto get_offset_index(int32_t offset) -> uint32_t
-        {
-            if (offset <= 2)
-            {
-                return offset - 1;
-            } else if (offset <= 4)
-            {
-                return 0 + (offset - 1) / 1;
-            } else if (offset <= 8)
-            {
-                return 2 + (offset - 1) / 2;
-            } else if (offset <= 16)
-            {
-                return 4 + (offset - 1) / 4;
-            } else if (offset <= 32)
-            {
-                return 6 + (offset - 1) / 8;
-            } else if (offset <= 64)
-            {
-                return 8 + (offset - 1) / 16;
-            } else if (offset <= 128)
-            {
-                return 10 + (offset - 1) / 32;
-            } else if (offset <= 256)
-            {
-                return 12 + (offset - 1) / 64;
-            } else if (offset <= 512)
-            {
-                return 14 + (offset - 1) / 128;
-            } else if (offset <= 1024)
-            {
-                return 16 + (offset - 1) / 256;
-            } else if (offset <= 2048)
-            {
-                return 18 + (offset - 1) / 512;
-            } else if (offset <= 4096)
-            {
-                return 20 + (offset - 1) / 1024;
-            } else if (offset <= 8192)
-            {
-                return 22 + (offset - 1) / 2048;
-            } else if (offset <= 16384)
-            {
-                return 24 + (offset - 1) / 4096;
-            } else if (offset <= 32768)
-            {
-                return 26 + (offset - 1) / 8192;
-            } else
-            {
-                return ~0u;    /* ~0 is an invalid distance code */
-            }
-        }
+        auto get_offset_index(int32_t offset) -> uint32_t;
     }
 
     template<class input_iterator_t,
@@ -159,7 +86,7 @@ namespace dpp
                 const int32_t match_length = internal::compare(candidate, current_byte);
                 const int32_t offset       = std::distance(candidate, current_byte);
 
-                if (match_length == -1)
+                if (match_length == -1 || offset > HISTORY_BUFFER_SIZE)
                 {
                     literals_matches_histogram[*current_byte]++;
                     hash_table[current_byte] = std::distance(src_begin, current_byte);
@@ -179,6 +106,11 @@ namespace dpp
                 hash_table[current_byte] = std::distance(src_begin, current_byte);
                 current_byte++;
             }
+        }
+
+        for (auto symbol = current_byte; symbol < src_end; symbol++)
+        {
+            literals_matches_histogram[*symbol]++;
         }
     }
 }
--- tests/simple_test.cpp
+++ tests/simple_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <array>
+#include <vector>
 #include <memory>
 
 #include <dpp.hpp>
@@ -28,27 +29,9 @@ int main()
     result.assign(std::istreambuf_iterator<char>(file),
                   std::istreambuf_iterator<char>());
 
-    std::array<int16_t, dpp::huff::MAX_ALPHABET_SIZE>         histogram{};
-    std::array<dpp::huff::code, dpp::huff::MAX_ALPHABET_SIZE> alphabet{};
+    std::vector<uint8_t> destination(result.size(), 0);
 
-    for (auto c : result)
-    {
-        histogram[c]++;
-    }
-
-    dpp::huff::build_huffman_alphabet(histogram, alphabet);
-
-    uint32_t bit_length = 0;
-
-    for (auto c : result)
-    {
-        bit_length += alphabet[c].code_length;
-    }
-
-    uint32_t byte_length = (bit_length / 8) + (bit_length % 8 ? 1 : 0);
-
-    std::cout << "Resulted byte length: " << byte_length << "\n";
-    std::cout << "Compression ratio: " << (float) result.size() / (float) byte_length << "\n";
+    dpp::deflate(std::begin(result), std::end(result), std::begin(destination), std::end(destination));
 
     return 0;
 }
