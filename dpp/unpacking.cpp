45cfbb53f4b308bc5959ccbe89b8f6d07f5eae88
Gigel773 <gigelbig@gmail.com>
2020-07-25 10:35:02 +0300

    [feature][util] Bit reverse implementation

--- dpp/unpacking.cpp
+++ dpp/unpacking.cpp
@@ -2,6 +2,47 @@
 
 namespace dpp
 {
+    namespace util
+    {
+        constexpr auto reverse_byte(uint8_t value) -> uint8_t
+        {
+            uint8_t result       = 0;
+            uint8_t bit_position = 1u << 7u;
+
+            for (int32_t i = 0; i < 8; i++)
+            {
+                if (value & 1u)
+                {
+                    result |= bit_position;
+                }
+
+                bit_position >>= 1u;
+                value >>= 1u;
+            }
+
+            return result;
+        }
+
+        constexpr auto build_reverse_table() -> std::array<uint8_t, 256>
+        {
+            std::array<uint8_t, 256> result{};
+
+            for (uint16_t i = 0; i < 256; i++)
+            {
+                result[i] = reverse_byte(static_cast<uint8_t>(i));
+            }
+
+            return result;
+        }
+
+        auto reverse_byte_table(uint8_t byte) -> uint8_t
+        {
+            static constexpr const auto table = build_reverse_table();
+
+            return table[byte];
+        }
+    }
+
     auto create_match_lengths_mapping(const std::array<huff::code, LITERALS_MATCH_LENGTHS_TABLE_SIZE> &alphabet)
     -> std::array<huff::code, 258>
     {
@@ -28,17 +69,18 @@ namespace dpp
                  interval < 4 && (current_length - 3) < result.size();
                  interval++, current_base++)
             {
-                const uint32_t upper_boundary = current_length + bits_to_step[current_bit_length];
-                uint16_t       current_bits   = 0;
+                const uint32_t upper_boundary    = current_length + bits_to_step[current_bit_length];
+                const uint8_t  actual_bit_length = current_bit_length + 1;
+                uint8_t        current_bits      = 0;
 
                 while (current_length <= upper_boundary && (current_length - 3) < result.size())
                 {
-                    const uint16_t code        = alphabet[current_base].code;
-                    const uint16_t code_length = alphabet[current_base].code_length;
+                    const uint16_t code          = alphabet[current_base].code;
+                    const uint16_t code_length   = alphabet[current_base].code_length;
+                    const uint8_t  reversed_bits = util::reverse_byte_table(current_bits) >> (8u - actual_bit_length);
 
-                    // TODO implement bit reverse
-                    const uint16_t new_code        = code | static_cast<uint16_t>(current_bits << code_length);
-                    const uint8_t  new_code_length = code_length + current_bit_length + 1;
+                    const uint16_t new_code        = code | static_cast<uint16_t>(reversed_bits << code_length);
+                    const uint8_t  new_code_length = code_length + actual_bit_length;
 
                     result[current_length - 3] = {new_code, new_code_length};
                     current_bits++;
