e78d779b217feb6d92794936db37dacc3c3a20a4
Gigel773 <gigelbig@gmail.com>
2020-07-25 09:53:27 +0300

    [refactoring][huffman] Minor changes in unpacking code

---
+++ dpp/unpacking.cpp
@@ -0,0 +1,57 @@
+#include "unpacking.hpp"
+
+namespace dpp
+{
+    auto create_match_lengths_mapping(const std::array<huff::code, LITERALS_MATCH_LENGTHS_TABLE_SIZE> &alphabet)
+    -> std::array<huff::code, 258>
+    {
+        constexpr uint32_t          bits_to_step[] = {1, 3, 7, 15, 31};
+        std::array<huff::code, 258> result{};
+
+        uint8_t  current_bit_length = 0;
+        uint32_t current_length     = 3;
+        uint32_t current_base       = 256;
+
+        // Complete initial part
+        while (current_base < 264)
+        {
+            result[current_length - 3] = alphabet[current_base];
+
+            current_base++;
+            current_length++;
+        }
+
+        // Complete main part
+        while (current_length - 3 < result.size() && current_bit_length < 5)
+        {
+            for (int32_t interval = 0;
+                 interval < 4 && (current_length - 3) < result.size();
+                 interval++, current_base++)
+            {
+                const uint32_t upper_boundary = current_length + bits_to_step[current_bit_length];
+                uint16_t       current_bits   = 0;
+
+                while (current_length <= upper_boundary && (current_length - 3) < result.size())
+                {
+                    const uint16_t code        = alphabet[current_base].code;
+                    const uint16_t code_length = alphabet[current_base].code_length;
+
+                    // TODO implement bit reverse
+                    const uint16_t new_code        = code | static_cast<uint16_t>(current_bits << code_length);
+                    const uint8_t  new_code_length = code_length + current_bit_length + 1;
+
+                    result[current_length - 3] = {new_code, new_code_length};
+                    current_bits++;
+                    current_length++;
+                }
+            }
+
+            current_bit_length++;
+        }
+
+        // Complete 258 length
+        result[current_base + 1] = alphabet.back();
+
+        return result;
+    }
+}
--- dpp/unpacking.hpp
+++ dpp/unpacking.hpp
@@ -6,55 +6,7 @@
 namespace dpp
 {
     auto create_match_lengths_mapping(const std::array<huff::code, LITERALS_MATCH_LENGTHS_TABLE_SIZE> &alphabet)
-    -> std::array<huff::code, 258>
-    {
-        constexpr uint32_t          bits_to_step[] = {1, 3, 7, 15, 31};
-        std::array<huff::code, 258> result{};
-
-        uint8_t  current_bit_length = 0;
-        uint32_t current_length     = 3;
-        uint32_t current_base       = 256;
-
-        // Complete initial part
-        while (current_base < 264)
-        {
-            result[current_length - 3] = alphabet[current_base];
-
-            current_base++;
-            current_length++;
-        }
-
-        // Complete main part
-        while (current_length - 3 < result.size() && current_bit_length < 5)
-        {
-            for (int32_t interval = 0;
-                 interval < 4 && (current_length - 3) < result.size();
-                 interval++, current_base++)
-            {
-                const uint32_t upper_boundary = current_length + bits_to_step[current_bit_length];
-                uint8_t        current_bits   = 0;
-
-                while (current_length <= upper_boundary && (current_length - 3) < result.size())
-                {
-                    // TODO implement bit reverse
-                    const uint16_t new_code        = alphabet[current_base].code |
-                                                     (current_bits << alphabet[current_base].code_length);
-                    const uint8_t  new_code_length = alphabet[current_base].code_length + current_bit_length + 1;
-
-                    result[current_length - 3] = {new_code, new_code_length};
-                    current_bits++;
-                    current_length++;
-                }
-            }
-
-            current_bit_length++;
-        }
-
-        // Complete 258 length
-        result[current_base + 1] = alphabet.back();
-
-        return result;
-    }
+    -> std::array<huff::code, 258>;
 }
 
 #endif //DPP_UNPACKING_HPP
