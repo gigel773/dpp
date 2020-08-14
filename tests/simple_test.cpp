e9176db7b5116ef9e4ab3588e3048130064d4ea1
Gigel773 <gigelbig@gmail.com>
2020-08-15 02:53:17 +0300

    [update][test] Use filesystem to iterate over the whole data corpus

--- tests/simple_test.cpp
+++ tests/simple_test.cpp
@@ -3,39 +3,41 @@
 #include <array>
 #include <vector>
 #include <memory>
+#include <filesystem>
 
 #include <dpp.hpp>
 
 #if WIN32
-constexpr char testFilePath[] = R"(..\..\testdata\bib)";
+constexpr char testFilePath[] = R"(..\..\testdata)";
 #else
-constexpr char testFilePath[] = R"(../../testdata/bib)";
+constexpr char testFilePath[] = R"(../../testdata)";
 #endif
 
 int main()
 {
-    std::ifstream file(testFilePath, std::ifstream::binary);
-    std::string   result;
-
-    if (!file.is_open())
+    for (auto &path: std::filesystem::directory_iterator(testFilePath))
     {
-        return 1;
+        std::ifstream file(path.path().string(), std::ifstream::binary);
+        std::string   result;
+
+        if (!file.is_open())
+        {
+            return 1;
+        }
+
+        result.reserve(path.file_size());
+
+        result.assign(std::istreambuf_iterator<char>(file),
+                      std::istreambuf_iterator<char>());
+
+        std::vector<uint8_t> destination(result.size(), 0);
+
+        auto result_size = dpp::deflate(std::begin(result), std::end(result), std::begin(destination));
+
+        std::cout << "\n[\t" << path.path().filename() << "\t]\n";
+        std::cout << "Compressed size: " << result_size << "\n";
+        std::cout << "Compression ratio: " << (float) (result.size()) / (float) (result_size) << "\n";
+        std::cout << "Compression %: " << ((float) (result_size) / (float) (result.size())) * 100 << "\n";
     }
-
-    file.seekg(0, std::ios::end);
-    result.reserve(static_cast<unsigned int>(file.tellg()));
-    file.seekg(0, std::ios::beg);
-
-    result.assign(std::istreambuf_iterator<char>(file),
-                  std::istreambuf_iterator<char>());
-
-    std::vector<uint8_t> destination(result.size(), 0);
-
-    auto result_size = dpp::deflate(std::begin(result), std::end(result), std::begin(destination));
-
-    std::cout << "Compressed size: " << result_size << "\n";
-    std::cout << "Compression ratio: " << (float) (result.size()) / (float) (result_size) << "\n";
-    std::cout << "Compression %: " << ((float) (result_size) / (float) (result.size())) * 100 << "\n";
-
     return 0;
 }
